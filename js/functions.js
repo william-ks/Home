b7f5f7909df2cae70c1713675f02dcf93a422013
William Krisley <43452688+william-ks@users.noreply.github.com>
2021-02-27 01:34:27 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+$(() => {
+
+    var target = $('.animes'),
+        animationClass = 'animate__animated animate__zoomIn',
+        windowHeight = $(window).height(),
+        offset = windowHeight - (windowHeight / 4);
+
+    
+    function OFF_SET_TOP(element){
+        var elOFFY = $(element).offset().top;
+        return elOFFY;
+    }
+
+    function animeScroll() {
+
+        var windowTop = $(window).scrollTop();
+
+        target.each(function() {
+
+            if (windowTop > (OFF_SET_TOP(this) - offset)) {
+                $(this).addClass(animationClass);
+            }
+
+        });
+
+    }
+
+    $(window).scroll(function() {
+        animeScroll();
+    });
+
+})
